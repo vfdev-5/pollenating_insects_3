e6c00294523ff439771c50555579a7f4c4761dbd
vfdev-5 <vfdev.5@gmail.com>
2017-10-16 08:38:01 +0200

    * Update results and DenseNet169 image preprocessor

--- submissions/keras_DenseNet169_finetunning_imbalanced2a/image_preprocessor.py
+++ submissions/keras_DenseNet169_finetunning_imbalanced2a/image_preprocessor.py
@@ -31,31 +31,35 @@ train_color_aug = iaa.Sequential([
 ])
 
 
-test_geom_aug = iaa.Sequential([
-    iaa.Fliplr(0.5),
-    iaa.Flipud(0.5),
-])
+# test_geom_aug = iaa.Sequential([
+#     iaa.Fliplr(0.5),
+#     iaa.Flipud(0.5),
+# ])
+test_geom_aug = None
+
+# test_color_aug = iaa.Sequential([
+#     iaa.OneOf([
+#         iaa.Add((-25, 25), per_channel=0.5),  # change brightness of images (by -10 to 10 of original value)
+#         iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5),  # improve or worsen the contrast
+#         iaa.AddToHueAndSaturation((-20, 20)),  # change hue and saturation
+#     ])
+# ])
+test_color_aug = None
 
 
-test_color_aug = iaa.Sequential([
-    iaa.OneOf([
-        iaa.Add((-25, 25), per_channel=0.5),  # change brightness of images (by -10 to 10 of original value)
-        iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5),  # improve or worsen the contrast
-        iaa.AddToHueAndSaturation((-20, 20)),  # change hue and saturation
-    ])
-])
-
-
-def _transform(x, geom_aug, color_aug):
+def _transform(x, geom_aug=None, color_aug=None):
     # RGBA -> RGB
     if x.shape[2] == 4:
         x = x[:, :, 0:3]
     
     # Resize to SIZE
     x = cv2.resize(x, dsize=SIZE, interpolation=cv2.INTER_CUBIC)
+
     # Data augmentation:
-    x = geom_aug.augment_image(x)
-    x = color_aug.augment_image(x)
+    if geom_aug is not None:
+        x = geom_aug.augment_image(x)
+    if color_aug is not None:
+        x = color_aug.augment_image(x)
 
     # to float32
     x = x.astype(np.float32)
